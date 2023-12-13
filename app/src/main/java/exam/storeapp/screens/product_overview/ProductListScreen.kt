2c9b0b9cdfcc0ecd7a30fe5c06e66d2565645c47
Ahrow <jonasbergenheim90@gmail.com>
2023-12-13 11:12:20 +0100

    merge produt item into product list screen to keep it consistent

--- app/src/main/java/exam/storeapp/screens/product_overview/ProductListScreen.kt
+++ app/src/main/java/exam/storeapp/screens/product_overview/ProductListScreen.kt
@@ -1,5 +1,7 @@
 package exam.storeapp.screens.product_overview
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,8 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.ShoppingCart
@@ -26,8 +30,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import exam.storeapp.data.Product
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -117,6 +128,68 @@ fun ProductListScreen(
                     }
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun ProductItem(
+    product: Product,
+    onClick: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 12.dp,
+                vertical = 8.dp
+            )
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(10)
+            )
+            .background(color = Color.White)
+            .clickable {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        AsyncImage(
+            modifier = Modifier
+                .size(108.dp, 108.dp)
+                .background(color = Color.Gray),
+            model = product.image,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            contentDescription = "Image of ${product.title}"
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = product.title,
+                fontWeight = FontWeight.SemiBold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Price: ${product.price}",
+                style = MaterialTheme.typography.labelMedium,
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Category: ${product.category}",
+                style = MaterialTheme.typography.labelMedium,
+                color = Color.Gray
+            )
         }
     }
 }
